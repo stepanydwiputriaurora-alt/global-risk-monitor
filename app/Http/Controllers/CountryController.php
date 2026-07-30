416fde43eb878869649f92d66bdebf0321c844a1
stepany <stepanydwiputriaurora@gmail.com>
2026-07-30 17:16:32 +0700

    news update

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -166,11 +166,6 @@ class CountryController extends Controller
         // Apakah DB negara masih kosong?
         $needsSync = count($countries) === 0;
 
-    dd([
-    'countries_count' => count($countries),
-    'selectedCountry' => $selectedCountry,
-    'country' => $country,
-        ]);
         return view('countries.index', [
             'countries'   => $countries,
             'country'     => $country,
